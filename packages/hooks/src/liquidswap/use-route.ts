6f1def56b66c63b7e26ded1bc8535774c99aab6b
cipher0 <cipher0.dev@outlook.com>
2025-07-03 12:10:27 +0800

    feat: increment version to 0.1.14 and add unwrapWHYPE parameter to useRoute hook

--- packages/hooks/src/liquidswap/use-route.ts
+++ packages/hooks/src/liquidswap/use-route.ts
@@ -6,12 +6,26 @@ export const useLiquidswapRoute = (
   tokenIn: Address,
   tokenOut: Address,
   amountIn: string,
+  unwrapWHYPE: boolean = false,
   slippage: number = 0.5,
 ): { data: RouteResponse | undefined; isLoading: boolean } => {
   const { data, isLoading } = useQuery({
-    queryKey: ["liquidswapRoute", tokenIn, tokenOut, amountIn, slippage],
+    queryKey: [
+      "liquidswapRoute",
+      tokenIn,
+      tokenOut,
+      amountIn,
+      unwrapWHYPE,
+      slippage,
+    ],
     queryFn: async () => {
-      return await liquidswap.getRoute(tokenIn, tokenOut, amountIn, slippage);
+      return await liquidswap.getRoute(
+        tokenIn,
+        tokenOut,
+        amountIn,
+        unwrapWHYPE,
+        slippage,
+      );
     },
     enabled: !!tokenIn && !!tokenOut && !!amountIn,
   });
